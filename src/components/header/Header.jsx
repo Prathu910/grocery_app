7d85a6a1636fec4ec16e433d75b33c2d26462d01
Prathamesh Trivedi <prathameshtrivedi9@gmail.com>
2023-07-13 09:49:27 +0530

    Added Responsiveness for Mob-Menu.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import Logo from "../../assets/Vector.png";
 import { BsSearch } from "react-icons/bs";
 import { GrCart } from "react-icons/gr";
 import { Link, useNavigate } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [showMobMenu, setShowMobMenu] = useState(false);
   return (
-    <div className="header-content container">
-      <Link to="/">
-        <div className="logo">
-          <img src={Logo} alt="logo" />
-          <h1>Logo</h1>
-        </div>
-      </Link>
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>Products</li>
-        <li>About us</li>
-      </ul>
-      <div className="right-section">
-        <div className="search-box">
-          <BsSearch />
-          <input type="text" />
-        </div>
-        <div className="cart">
-          <button onClick={() => navigate("/cart")}>
-            <span>{<GrCart />}</span>My Cart
-          </button>
+    <>
+      <div className="header-content container">
+        <Link to="/">
+          <div className="logo">
+            <img src={Logo} alt="logo" />
+            <h1>Logo</h1>
+          </div>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>Products</li>
+          <li>About us</li>
+        </ul>
+        <div className="right-section">
+          <div className="search-box">
+            <BsSearch />
+            <input type="text" />
+          </div>
+          <div className="cart">
+            <button onClick={() => navigate("/cart")}>
+              <span>{<GrCart />}</span>My Cart
+            </button>
+          </div>
+          <div className="bars" onClick={() => setShowMobMenu(!showMobMenu)}>
+            <span>
+              <FaBars />
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={`mob-menu ${showMobMenu ? "" : "hidden"}`}>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>Products</li>
+          <li>About us</li>
+        </ul>
+      </div>
+    </>
   );
 };
 
